4c99f626b42d75d32249f96c887e8405feff25e9
Khondaker Tasnia Hoque <60292796+Tasnia16@users.noreply.github.com>
2021-05-21 07:17:18 -0700

    Add files via upload

---
+++ Project-Shop-Management-main/Shop Management/src/sample/LoginController.java
@@ -0,0 +1,37 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class LoginController {
+    @FXML
+    private Label lblStatus;
+
+    @FXML
+    private TextField txtUserName;
+
+    @FXML
+    private TextField txtPass;
+
+    public void login(ActionEvent event) throws IOException {
+        if(txtUserName.getText().equals("User") && txtPass.getText().equals("1234"))
+        {
+            lblStatus.setText("Login Successful!");
+            Parent FrontPageController2 = FXMLLoader.load(getClass().getResource("/sample/FrontPageController.fxml"));
+            Scene FrontPageScene = new Scene(FrontPageController2);
+            Stage FrontPageStage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+            FrontPageStage.setTitle("Shop Management");
+            FrontPageStage.setScene(FrontPageScene);
+            FrontPageStage.show();
+        }
+        else lblStatus.setText("Login Failed!");
+    }
+}
---
+++ Project-Shop-Management-main/Shop Management/src/sample/Main.java
@@ -0,0 +1,29 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/sample/Login.fxml"));
+            primaryStage.setTitle("Shop Management");
+            primaryStage.setScene(new Scene(root, 600, 600));
+            primaryStage.show();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
