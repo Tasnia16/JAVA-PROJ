2034848154373335d1fe810bfdde873a6fc5d362
Khondaker Tasnia Hoque <60292796+Tasnia16@users.noreply.github.com>
2021-05-22 01:46:43 -0700

    Add files via upload

---
+++ sample/FrontPageController.java
@@ -0,0 +1,30 @@
+package sample;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class FrontPageController {
+    @FXML
+    private Button btnGenerate;
+    @FXML
+    private void handleButtonAction(ActionEvent event) throws IOException {
+        if(event.getSource()==btnGenerate){
+            Parent FrontPageController3 = FXMLLoader.load(getClass().getResource("/sample/Controller2.fxml"));
+            Scene FrontPageScene3 = new Scene(FrontPageController3);
+            Stage FrontPageStage3 = (Stage) ((Node)event.getSource()).getScene().getWindow();
+            FrontPageStage3.setTitle("Shop Management");
+            FrontPageStage3.setScene(FrontPageScene3);
+            FrontPageStage3.show();
+        }
+    }
+
+    }
+
